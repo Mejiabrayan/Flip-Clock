f004f86c67723689b19566126e10a4ccd0fb9708
mejiabrayan <learnwithbrayan@gmail.com>
2022-01-30 16:20:30 -0800

    update app.js line 54

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-
+// Our constructor function
 var Flipper = function() {
     function Flipper(node, currentTime, nextTime) {
           this.isFlipping = false;
           this.duration = 600;
           this.flipNode = node;
-          this.frontNode = node.querySelector(".front");
-          this.backNode = node.querySelector(".back");
-          this.setFrontTime(currentTime);
-          this.setBackTime(nextTime);
+          this.frontNode = node.querySelector(".front"); // Gets the front element
+          this.backNode = node.querySelector(".back"); // Gets the back element
+          this.setFrontTime(currentTime); // Our methods 
+          this.setBackTime(nextTime); // Method
     }
     Flipper.prototype.setFrontTime = function (time) {
         this.frontNode.dataset.number = time;
@@ -39,16 +39,19 @@ var getTimeFromDate = function (date) {
     return date.toTimeString().slice(0, 8).split(":").join("");
 };
 
+
+// Our Count 
 var flips = document.querySelectorAll(".flip");
 var now = new Date();
 var nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
 var nextTimeStr = getTimeFromDate(now);
-var Flippers = Array.from(flips).map(function(flip, i) {
+var Flippers = Array.from(flips).map(function(flip, i) { // Grabs all Flips
     return new Flipper(flip, nowTimeStr[i], nextTimeStr[i]);
 })
 setInterval(function() {
     var now = new Date();
     var nowTimeStr = getTimeFromDate(new Date(now.getTime() - 1000));
+    var nextTimeStr = getTimeFromDate(now);
 
     for (var i = 0; i < Flippers.length; i++) {
         if (nowTimeStr[i] === nextTimeStr[i]){
@@ -56,4 +59,4 @@ setInterval(function() {
         }
         Flippers[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
     }
-}, 1000)
+}, 1000);
